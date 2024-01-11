3056f47a7ecd8c31685a5652a623b3142ec72025
Eng Heng <enghengyeo12@Hotmail.com>
2024-01-11 13:10:57 +0800

    tweet embedding

--- schemas/objects/twitter.js
+++ schemas/objects/twitter.js
@@ -1,4 +1,15 @@
 import { FaTwitter } from "react-icons/fa";
+import React from 'react'
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
+const Preview = ({ value }) => {
+    const { id } = value;
+    return (
+      <div>
+        {id ? <TwitterTweetEmbed tweetId={id} /> : <p>No tweet ID provided</p>}
+      </div>
+    );
+  }
 
 const twitter = {
     name: 'twitter',
@@ -6,12 +17,18 @@ const twitter = {
     title: 'Twitter Embed',
     icon:FaTwitter,
     fields: [
-      {
-        name: 'id',
-        type: 'string',
-        title: 'Twitter tweet ID'
+        {
+          name: 'id',
+          type: 'string',
+          title: 'Twitter tweet id'
+        }
+      ],
+      preview: {
+        select: {
+           id: 'id'
+        },
+        component: Preview
       }
-    ]
   }
 
   export default twitter
